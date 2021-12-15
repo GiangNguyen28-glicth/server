3fbae61a6394bb1f698ac460d33d48d510f61c54
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2021-12-15 23:41:54 +0700

    update

--- src/User/DTO/user.dto.ts
+++ src/User/DTO/user.dto.ts
@@ -7,7 +7,7 @@ export class UserDTO{
     @IsString()
     lastName:string;
     @IsString()
-    @Matches(/^(?=.*[\a-zA-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
+    @Matches(/^(?=.*[a-zA-z\d@$!%*#?&.=])(?=.*\d)(?=.*[@$!%*#?&.=])[A-Za-z\d@$!%*#?&.=]{8,}$/, {
         message: '8 characters including 1 uppercase letter, 1 special character',
     })
     password:string;
