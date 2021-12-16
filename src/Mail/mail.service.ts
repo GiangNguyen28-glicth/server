91ba2689c45c707c47869db7c184668d5999c08c
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2021-12-16 12:27:09 +0700

    update

--- src/Mail/mail.service.ts
+++ src/Mail/mail.service.ts
@@ -2,39 +2,18 @@ import { BadRequestException, forwardRef, Inject, Injectable } from "@nestjs/com
 import { JwtService } from "@nestjs/jwt";
 import { UserService } from "src/User/User.service"
 import * as nodemailer from 'nodemailer';
-import { google } from "googleapis";
 
 @Injectable()
 export class MailService{
     constructor(@Inject(forwardRef(() => UserService)) private userService: UserService,private jwtservice:JwtService){}
-    oAuth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      process.env.GOOGLE_CLIENT_REDIRECT_URI
-    );
   
     async sendEmail(email:string): Promise<void>{
-        this.oAuth2Client.setCredentials({refresh_token:process.env.GOOGLE_CLIENT_REFRESH_TOKEN});
-        const accessToken = await this.oAuth2Client.getAccessToken(); 
         const payload={email};
         const token = this.jwtservice.sign(payload, {
         secret: process.env.JWT_VERIFICATION_TOKEN_SECRET,
         expiresIn: `${process.env.JWT_VERIFICATION_TOKEN_EXPIRATION_TIME}s`
         });
         const url = `${process.env.EMAIL_CONFIRMATION_URL}?token=${token}`;
-        // const transporter = nodemailer.createTransport({
-        //   service: 'gmail',
-        //   port:600,
-        //   secure:true,
-        //   auth: {
-        //     type: 'OAuth2',
-        //     user: '103tmdt@gmail.com',
-        //     clientId:process.env.GOOGLE_CLIENT_ID,
-        //     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        //     refreshToken:process.env.GOOGLE_CLIENT_REFRESH_TOKEN,
-        //     accessToken:accessToken,
-        //   },
-        // });
         const transporter = nodemailer.createTransport({
           service: "gmail",
           port:600,
