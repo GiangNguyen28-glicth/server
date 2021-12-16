7f2cd1197fa22e8d7d78d149bc5084c84c810196
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2021-12-16 19:46:34 +0700

    update

--- dist/User/User.service.js
+++ dist/User/User.service.js
@@ -104,6 +104,8 @@ let UserService = class UserService {
     async Login({ email, password }) {
         const user = await this.usermodel.findOne({ email: email });
         if (user && (await bcrypt.compare(password, user.password)) && user.isEmailConfirmed) {
+            const OTP = await this.randomOTP();
+            await this.sendSMS(user.phoneNumber, OTP.toString());
             return {
                 code: 200, success: true, message: "Check otp"
             };
