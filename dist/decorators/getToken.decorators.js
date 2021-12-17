ee48e4f2ab4292177f23eb03779ca7f9de52f5ff
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2021-12-17 14:40:26 +0700

    up

--- dist/decorators/getToken.decorators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetToken = void 0;
-const common_1 = require("@nestjs/common");
-exports.GetToken = (0, common_1.createParamDecorator)(async (data, ctx) => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.headers.authorization.split(" ")[1];
-});
-//# sourceMappingURL=getToken.decorators.js.map
