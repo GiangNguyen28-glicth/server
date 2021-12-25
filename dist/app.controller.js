4c0839eed5614ca406d95ea2acb0ad7caa78b85b
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2021-12-25 20:22:19 +0700

    up

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -13,7 +13,7 @@ exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
 let AppController = class AppController {
     async checkServer() {
-        return "Deploy Success Version 7";
+        return "Deploy Success Version 10";
     }
 };
 __decorate([
